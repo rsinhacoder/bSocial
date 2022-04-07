cf01dacd456a3e0c9c65200b44d97a28c38e01d2
ritik sinha <ritik210798@gmail.com>
2022-04-07 14:58:19 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
 import userRouter from './routes/user.js';
@@ -17,10 +17,10 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://ritiksinha:Noodlesoup@cluster0.hr3yk.mongodb.net/mernsocial?retryWrites=true&w=majority';
+dotenv.config();
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect( process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`did not connect`));
 
